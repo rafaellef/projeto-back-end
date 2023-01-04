190993d8cac66e6e1016fa7f055764392e4c6747
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2023-01-04 17:29:09 -0300

    Conectando banco de dados

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco
+mongoose.connect('mongodb+srv://rafael:<password>@cluster0.wpnekdw.mongodb.net/?retryWrites=true&w=majority');
 
 // Carrega as Rotas
 const indexRoute = require("./routes/index-route");
