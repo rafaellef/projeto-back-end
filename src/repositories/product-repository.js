c1db5f1a5c3cce3a9b75751f9720ce5afb03b4d2
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2023-01-08 21:31:52 -0300

    Implementando Async Await

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -2,46 +2,50 @@
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 
-exports.get = () => {
-  return Product.find(
+exports.get = async () => {
+  const res = await Product.find(
     {
       active: true,
     },
     "title price slug"
   );
+  return res;
 };
 
-exports.getBySlug = (slug) => {
-  return Product.findOne(
+exports.getBySlug = async (slug) => {
+  const res = await Product.findOne(
     {
       slug: slug,
       active: true,
     },
     "title description price slug tags"
   );
+  return res;
 };
 
-exports.getById = (id) => {
-  return Product.findById(id);
+exports.getById = async (id) => {
+  const res = await Product.findById(id);
+  return res;
 };
 
-exports.getByTag = (tag) => {
-  return Product.find(
+exports.getByTag = async(tag) => {
+  const res = Product.find(
     {
       tags: tag,
       active: true,
     },
     "title description price slug tags"
   );
+  return res;
 };
 
-exports.create = (data) => {
+exports.create = async(data) => {
   var product = new Product(data);
-  return product.save();
+  await product.save();
 };
 
-exports.update = (id, data) => {
-  return Product.findByIdAndUpdate(id, {
+exports.update = async(id, data) => {
+  await Product.findByIdAndUpdate(id, {
     $set: {
       title: data.title,
       description: data.description,
@@ -51,6 +55,6 @@ exports.update = (id, data) => {
   });
 };
 
-exports.delete = (id) => {
-  return Product.findOneAndRemove(id);
+exports.delete = async(id) => {
+  await Product.findOneAndRemove(id);
 };
